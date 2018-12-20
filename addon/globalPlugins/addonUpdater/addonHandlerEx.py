e6f2cfe28ba6492072acdca7df3b724aece27d53
Joseph Lee <joseph.lee22590@gmail.com>
2018-12-20 09:54:43 -0800

    Add Columns Review

--- addon/globalPlugins/addonUpdater/addonHandlerEx.py
+++ addon/globalPlugins/addonUpdater/addonHandlerEx.py
@@ -31,6 +31,7 @@ names2urls={
 	"clipContentsDesigner": "https://addons.nvda-project.org/files/get.php?file=ccd",
 	"clipspeak": "https://addons.nvda-project.org/files/get.php?file=cs",
 	"clock": "https://addons.nvda-project.org/files/get.php?file=cac",
+	"columnsReview": "https://addons.nvda-project.org/files/get.php?file=cr",
 	"dayOfTheWeek": "https://addons.nvda-project.org/files/get.php?file=dw",
 	"dropbox": "https://addons.nvda-project.org/files/get.php?file=dx",
 	"easyTableNavigator": "https://addons.nvda-project.org/files/get.php?file=etn",
