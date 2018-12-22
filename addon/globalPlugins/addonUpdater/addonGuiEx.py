0975cfdc713e621f2856278bb005633aa4d6c016
Joseph Lee <joseph.lee22590@gmail.com>
2018-12-22 08:53:19 -0800

    Update downloader: deal with Python 2 and 3 differences in remote socket.
    Python 2 needs to set timeout in socket object, whereas Python 3 can set timeout from urlopen function. So take care of differences through importing sys module and checking Python version.

--- addon/globalPlugins/addonUpdater/addonGuiEx.py
+++ addon/globalPlugins/addonUpdater/addonGuiEx.py
@@ -214,13 +214,17 @@ class AddonUpdateDownloader(updateCheck.UpdateDownloader):
 		self.continueUpdatingAddons()
 
 	def _download(self, url):
-		remote = urlopen(url)
+		import sys
+		if sys.version_info.major == 3:
+			remote = urlopen(url, timeout=120)
+		else:
+			remote = urlopen(url)
 		if remote.code != 200:
 			raise RuntimeError("Download failed with code %d" % remote.code)
 		# #2352: Some security scanners such as Eset NOD32 HTTP Scanner
 		# cause huge read delays while downloading.
 		# Therefore, set a higher timeout.
-		remote.fp._sock.settimeout(120)
+		if sys.version_info.major == 2: remote.fp._sock.settimeout(120)
 		size = int(remote.headers["content-length"])
 		local = open(self.destPath, "wb")
 		if self.fileHash:
