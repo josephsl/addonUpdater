c2303d0fab09d61c96e69086b6e67ae4aee1ca3c
Joseph Lee <joseph.lee22590@gmail.com>
2020-09-10 11:14:56 -0700

    NVDA compatibility: last tested version -> 2020.4

--- buildVars.py
+++ buildVars.py
@@ -27,7 +27,7 @@ addon_info = {
 	# Minimum NVDA version supported
 	"addon_minimumNVDAVersion": "2020.2.0",
 	# Last NVDA version supported/tested
-	"addon_lastTestedNVDAVersion": "2020.3.0",
+	"addon_lastTestedNVDAVersion": "2020.4.0",
 	# Add-on update channel (default is stable)
 	"addon_updateChannel": None,
 }
