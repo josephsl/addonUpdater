5c5b159f69021c7d55c19a501319545d35600001
Joseph Lee <joseph.lee22590@gmail.com>
2021-06-19 02:00:06 -0700

    Add-on update toast: typo

--- addon/globalPlugins/addonUpdater/addonHandlerEx.py
+++ addon/globalPlugins/addonUpdater/addonHandlerEx.py
@@ -317,9 +317,9 @@ def _showAddonUpdateUI():
 			updateMessage = _(
 				# Translators: presented as part of add-on update notification message.
 				"One or more add-on updates are available. "
-				"Go to NVDA menu, Tools, Check for add-on update to review them."
+				"Go to NVDA menu, Tools, Check for add-on updates to review them."
 			)
-			# Translators: title of the add-on update notificaiton message.
+			# Translators: title of the add-on update notification message.
 			wx.adv.NotificationMessage(_("NVDA add-on updates"), updateMessage).Show(timeout=30)
 			_updateInfo = info
 		else:
