d5bd0013ab1ad2b5a63d09c75be468526192fa38
Joseph Lee <joseph.lee22590@gmail.com>
2021-10-02 05:43:44 -0700

    Update check: use Edge 94 user agent when fetching custom update URL's

--- addon/globalPlugins/addonUpdater/addonHandlerEx.py
+++ addon/globalPlugins/addonUpdater/addonHandlerEx.py
@@ -237,7 +237,7 @@ def fetchAddonInfo(info, results, addon, manifestInfo, addonsData):
 		req = Request(
 			f"https://addons.nvda-project.org/files/get.php?file={addonKey}",
 			headers={
-				"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36 Edg/92.0.902.78"  # NOQA: E501
+				"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36 Edg/94.0.992.31"  # NOQA: E501
 			}
 		)
 		try:
