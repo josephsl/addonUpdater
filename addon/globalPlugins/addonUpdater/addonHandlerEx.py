a9973b582af91e7b617f5a21ede158c30b62d057
Joseph Lee <joseph.lee22590@gmail.com>
2021-12-25 11:29:33 -0800

    Update copyright year

--- addon/globalPlugins/addonUpdater/addonHandlerEx.py
+++ addon/globalPlugins/addonUpdater/addonHandlerEx.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 # addonHandler.py
 # A part of NonVisual Desktop Access (NVDA)
-# Copyright (C) 2012-2019 Rui Batista, NV Access Limited, Noelia Ruiz Martínez, Joseph Lee
+# Copyright (C) 2012-2022 Rui Batista, NV Access Limited, Noelia Ruiz Martínez, Joseph Lee
 # This file is covered by the GNU General Public License.
 # See the file COPYING for more details.
 
