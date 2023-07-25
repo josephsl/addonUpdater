aea4d883a5e73ddfda2ff39bfffd0a030c703bbe
Joseph Lee <joseph.lee22590@gmail.com>
2023-07-25 00:10:21 -0700

    Introduce install tasks for reminding users about add-on store notification.
    Set add-on store notification shown flag to False when installing Add-on Updater (at least 23.08).

---
+++ addon/installTasks.py
@@ -0,0 +1,15 @@
+# addonUpdater/installTasks.py
+# Copyright 2023 Joseph Lee, released under GPL.
+
+# Provides needed routines during add-on installation and removal.
+# Mostly checks compatibility.
+# Partly based on other add-ons, particularly Place Markers by Noelia Martinez (thanks add-on authors).
+
+import addonHandler
+addonHandler.initTranslation()
+
+
+def onInstall():
+	# Display add-on store notification after restarting NVDA.
+	import globalPlugins
+	globalPlugins.addonUpdater.addonUtils.updateState["addonStoreNotificationShown"] = False
