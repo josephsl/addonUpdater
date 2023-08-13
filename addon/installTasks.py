3f5c6c0b929f707bd94554f48027409d1eb6a1d9
Joseph Lee <joseph.lee22590@gmail.com>
2023-08-13 08:31:34 -0600

    Install tasks: present add-on store message when installing the add-on.
    Instead of a startup message, present add-on store message (including NVDA 2023.1) at install time. This simplifies add-on startup routine and prepares to support Add-on Updater itself being added to legacy add-ons list.

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -43,3 +43,19 @@ def onInstall():
 				), unsupportedWindowsReleaseTitle, wx.OK | wx.ICON_ERROR
 			)
 		raise RuntimeError("Attempting to install Add-on Updater add-on on Windows releases earlier than 10")
+	# Present add-on store messages while installing this add-on.
+	addonStoreMessage = _(
+		# Translators: message presented when add-on store is available in NVDA.
+		"You are using NVDA 2023.1 or earlier. NVDA 2023.2 introduces an add-on store "
+		"to browse, install, manage, and update add-ons. After updating to NVDA 2023.2 or later, "
+		"Visit NVDA add-on store (NVDA menu, Tools, add-on store) to check for add-on updates. "
+		"Add-on Updater can still be used to check for add-on updates in the meantime."
+	)
+	if hasattr(gui.mainFrame, "onAddonStoreCommand"):
+		addonStoreMessage = _(
+			# Translators: message presented when add-on store is available in NVDA.
+			"You are using an NVDA release with add-on store included. "
+			"Visit NVDA add-on store (NVDA menu, Tools, add-on store) to check for add-on updates. "
+			"Add-on Updater can still be used to check for add-on updates in the meantime."
+		)
+	gui.messageBox(addonStoreMessage, _("Add-on Updater"), wx.OK | wx.ICON_INFORMATION)
