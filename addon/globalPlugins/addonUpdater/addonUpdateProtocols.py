9b90f0b9dd6b90318b0ddf30e030c6e27f75f149
Joseph Lee <joseph.lee22590@gmail.com>
2024-12-28 08:41:52 -0700

    Add-on update protocols: update Edge user agent to 132.0.2957.55

--- addon/globalPlugins/addonUpdater/addonUpdateProtocols.py
+++ addon/globalPlugins/addonUpdater/addonUpdateProtocols.py
@@ -35,7 +35,7 @@ def getUrlViaMSEdgeUserAgent(url: str) -> Request:
 	return Request(
 		url,
 		headers={
-			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.1938.62"  # NOQA: E501
+			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36 Edg/132.0.2957.55"  # NOQA: E501
 		},
 	)
 
